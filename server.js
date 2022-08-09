f14d0d9e77cda14412bf05001d210512c7af7af2
donald <donald.github@mechos.net>
2022-08-09 22:12:36 +0100

    ready to consume WoT objects, need discovery step

--- server.js
+++ server.js
@@ -7,15 +7,53 @@ const servient = new Servient();
 servient.addClientFactory(new HttpClientFactory(null));
 const WoTHelpers = new Helpers(servient);
 
-WoTHelpers.fetch("http://localhost:8080/example").then(async (td) => {
-    try {
-        servient.start().then(async (WoT) => {
-            // Then from here on you can consume the thing
-            // i.e let thing = await WoT.consume(td) ...
-        });
-    }
-    catch (err) {
-        console.error("Script error:", err);
-    }
-}).catch((err) => { console.error("Fetch error:", err); });
+const weathercontrolthingPromise = new Promise ( (myResolve, myReject) => {
+    WoTHelpers.fetch("http://192.168.86.13:8080/weathercontrolthing").then(async (td) => {
+        try {
+            servient.start().then(async (WoT) => {
+                // Then from here on you can consume the thing
+                // i.e let thing = await WoT.consume(td) ...
+                myResolve(WoT);
+            });
+        }
+        catch (err) {
+            console.error("Script error:", err);
+            myReject(err);
+        }
+    }).catch((err) => { 
+        console.error("Fetch error:", err);  
+        myReject(err) });
+})
 
+const sensornodethingPromise = new Promise ( (myResolve, myReject) => {
+    WoTHelpers.fetch("http://192.168.86.251:8080/sensehatenv").then(async (td) => {
+        try {
+            servient.start().then(async (WoT) => {
+                // Then from here on you can consume the thing
+                // i.e let thing = await WoT.consume(td) ...
+                myResolve(WoT);
+            });
+        }
+        catch (err) {
+            console.error("Script error:", err);
+            myReject(err);
+        }
+    }).catch((err) => { 
+        console.error("Fetch error:", err);  
+        myReject(err) });
+})
+
+weathercontrolthingPromise.then( 
+    (weatherWoT) => {
+        sensornodethingPromise.then(
+            (sensorWoT) => {
+                console.log("Promises fulfilled");
+                console.log(weatherWoT);
+                console.log(sensorWoT);
+                
+            },
+            (err) => {console.log(err);}
+        )
+    },
+    (err) => { console.log(err);} 
+)
