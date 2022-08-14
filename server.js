24b96f6646af22510899f623ebaad08a9fb1944d
tuath-pi <pi@example.com>
2022-08-14 16:01:04 +0100

    working

--- server.js
+++ server.js
@@ -1,122 +1,114 @@
 const mDnsSd = require('node-dns-sd');
 const LinkSmartThingDirectory = require('link_smart_thing_directory');
 
-
-
-const directorySearchApiPromise = new Promise ((myResolve, myReject) => {
-    mDnsSd.discover({
-        name: '_wot._tcp.local'
-        }).then((device_list) =>{
-            const directory = {};  // only one directory handled for now
-            device_list.forEach(element => {
-                directory.address = element.address;
-                directory.port = element.service.port; 
-            });
-            const apiClient = new LinkSmartThingDirectory.ApiClient(directory.address+":"+directory.port);
-            const searchApi = new LinkSmartThingDirectory.SearchApi(apiClient);
-            myResolve(searchApi);
-        }).catch((error) => {
-          console.error(error);
-          myReject(error);
-        });
-
-})
-
-const weathercontrolDirectoryPromise = new Promise((myResolve,myReject) => {
-    directorySearchApiPromise.then( (searchApi) => {
-        //searchApi.searchXpathGet
-        searchApi.searchJsonpathGet("$[?(@.title=='WeatherControlThing')]", (error, data,response ) => {
-            if (error) {
-                console.error(error);
-                myReject(error);
-              } else {
-                myResolve(JSON.parse(response.text)[0]);
-              }
-        })
-    } )
-
-})
-
-const sensehatDirectoryPromise = new Promise((myResolve,myReject) => {
-    directorySearchApiPromise.then( (searchApi) => {
-        //searchApi.searchXpathGet
-        searchApi.searchJsonpathGet("$[?(@.title=='SenseHatEnv')]", (error, data,response ) => {
-            if (error) {
-                console.error(error);
-                myReject(error);
-              } else {
-                myResolve(JSON.parse(response.text)[0]);
-              }
-        })
-    } )
-
-})
-
-weathercontrolDirectoryPromise.then((td) => {
-    console.log(td);
-})
-
-sensehatDirectoryPromise.then((td) => {
-    console.log(td);
-})
-
-//
-// TODO //////////  use these Promises in place of WoTHelpers.fetch below //////
-
 const { Servient, Helpers } = require("@node-wot/core");
 const { HttpClientFactory } = require('@node-wot/binding-http');
 
-const servient = new Servient();
-servient.addClientFactory(new HttpClientFactory(null));
-const WoTHelpers = new Helpers(servient);
+const msPoll = 1000;
+const humidityThreshold = 34.0;
+const temperatureThreshold = 39.0;
 
-const weathercontrolthingPromise = new Promise ( (myResolve, myReject) => {
-    WoTHelpers.fetch("http://192.168.86.13:8080/weathercontrolthing").then(async (td) => {
-        try {
-            servient.start().then(async (WoT) => {
-                // Then from here on you can consume the thing
-                // i.e let thing = await WoT.consume(td) ...
-                myResolve(WoT);
+main();
+
+async function main(){
+
+    const directorySearchApiPromise = new Promise ((myResolve, myReject) => {
+        mDnsSd.discover({
+            name: '_wot._tcp.local'
+            }).then((device_list) =>{
+                const directory = {};  // only one directory handled for now
+                device_list.forEach(element => {
+                    directory.address = element.address;
+                    directory.port = element.service.port; 
+                });
+                const apiClient = new LinkSmartThingDirectory.ApiClient(directory.address+":"+directory.port);
+                const searchApi = new LinkSmartThingDirectory.SearchApi(apiClient);
+                myResolve(searchApi);
+            }).catch((error) => {
+            console.error(error);
+            myReject(error);
             });
-        }
-        catch (err) {
-            console.error("Script error:", err);
-            myReject(err);
-        }
-    }).catch((err) => { 
-        console.error("Fetch error:", err);  
-        myReject(err) });
-})
 
-const sensornodethingPromise = new Promise ( (myResolve, myReject) => {
-    WoTHelpers.fetch("http://192.168.86.251:8080/sensehatenv").then(async (td) => {
-        try {
-            servient.start().then(async (WoT) => {
-                // Then from here on you can consume the thing
-                // i.e let thing = await WoT.consume(td) ...
-                myResolve(WoT);
-            });
-        }
-        catch (err) {
-            console.error("Script error:", err);
-            myReject(err);
-        }
-    }).catch((err) => { 
-        console.error("Fetch error:", err);  
-        myReject(err) });
-})
+    })
 
-weathercontrolthingPromise.then( 
-    (weatherWoT) => {
-        sensornodethingPromise.then(
-            (sensorWoT) => {
-                console.log("Promises fulfilled");
-                console.log(weatherWoT);
-                console.log(sensorWoT);
-                
-            },
-            (err) => {console.log(err);}
-        )
-    },
-    (err) => { console.log(err);} 
-)
+    const weathercontrolDirectoryPromise = new Promise((myResolve,myReject) => {
+        directorySearchApiPromise.then( (searchApi) => {
+            //searchApi.searchXpathGet
+            searchApi.searchJsonpathGet("$[?(@.title=='WeatherControlThing')]", (error, data,response ) => {
+                if (error) {
+                    console.error(error);
+                    myReject(error);
+                } else {
+                    myResolve(JSON.parse(response.text)[0]);
+                }
+            })
+        } )
+
+    })
+
+    const sensehatDirectoryPromise = new Promise((myResolve,myReject) => {
+        directorySearchApiPromise.then( (searchApi) => {
+            //searchApi.searchXpathGet
+            searchApi.searchJsonpathGet("$[?(@.title=='SenseHatEnv')]", (error, data,response ) => {
+                if (error) {
+                    console.error(error);
+                    myReject(error);
+                } else {
+                    myResolve(JSON.parse(response.text)[0]);
+                }
+            })
+        } )
+
+    })
+
+
+    let weathercontrolTd,sensehatTd;
+    [weathercontrolTd,sensehatTd] = await Promise.all([weathercontrolDirectoryPromise,sensehatDirectoryPromise]);
+
+
+    const servient = new Servient();
+    servient.addClientFactory(new HttpClientFactory(null));
+    let WoT = await servient.start()
+
+    let weathercontrolThing, sensehatThing
+    weathercontrolThing = await WoT.consume(weathercontrolTd);
+    sensehatThing = await WoT.consume(sensehatTd);
+
+    let lastDry = true;
+    let lastCool = true;
+    await weathercontrolThing.invokeAction("clear");
+    setInterval(  ( async () => {
+        let temperature = await ( await sensehatThing.readProperty("temperature")).value();
+        let humidity = await ( await sensehatThing.readProperty("humidity")).value();
+
+        let dry = humidity < humidityThreshold;
+        let cool = temperature < temperatureThreshold;
+        console.log(JSON.stringify({humidity: humidity, 
+                                    temperature: temperature,
+                                    dry: dry,
+                                    cool: cool
+                                }));
+
+
+        let warming = ( lastCool && !cool );
+        let cooling = ( !lastCool && cool );
+        let moistening = ( lastDry && !dry );
+        let drying = ( !lastDry && dry );
+
+        if (drying) {
+            await weathercontrolThing.invokeAction("clear");
+        } //clear
+
+        if ( (cooling && !dry) || (moistening && cool)) { 
+            await weathercontrolThing.invokeAction("rain");
+        } //rain
+
+        if ( (warming && !dry) || (moistening && !cool)) {
+            await weathercontrolThing.invokeAction("thunder");
+        } //thunder
+
+        lastDry = dry;
+        lastCool = cool;
+    })
+    ,msPoll);
+}
